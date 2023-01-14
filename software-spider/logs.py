d12646e45fb5850968a886345602023f769644d3
zhengzongwei <zhengzongwei@foxmail.com>
2023-01-15 00:15:42 +0800

    支持下载git

---
+++ software-spider/logs.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+import os
+from logging import handlers
+import configparser
+
+"""
+################################################################
+# description: 日志记录模块
+# author: zhengzongwei@foxmail.com
+################################################################
+"""
+
+
+class LoggerBase(object):
+    """
+    Log Baase class
+    """
+
+    def __init__(self, logger_name) -> None:
+        # 是否控制台输出
+        self.base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        self.log_dir = None
+        self.logger_name = logger_name
+        self.LOG_LEVEL = None
+        self.LOG_FORMAT = None
+        self.console_log_status = True
+        self.stacklevel = 2
+
+        self.config_log()
+
+    @staticmethod
+    def check_log_dir(path) -> bool:
+        """
+        检查日志目录是否存在，不存在则创建
+        :return:
+        """
+        return os.path.exists(path)
+
+    @staticmethod
+    def mkdir_log_dir(path) -> None:
+        """
+        创建日志目录
+        :return:
+        """
+        if os.path.isfile(path):
+            path = os.path.abspath(path)
+        return os.makedirs(path)
+
+    def parse_log_level(self, log_level):
+        match log_level:
+            case 'debug':
+                self.LOG_LEVEL = logging.DEBUG
+            case 'info':
+                self.LOG_LEVEL = logging.INFO
+            case 'warning':
+                self.LOG_LEVEL = logging.WARNING
+            case 'error':
+                self.LOG_LEVEL = logging.ERROR
+            case 'critical':
+                self.LOG_LEVEL = logging.CRITICAL
+            case _:
+                self.LOG_LEVEL = logging.INFO
+
+    def parse_config(self):
+        cf = configparser.RawConfigParser()
+        cf.read("./log.conf")
+        options = cf.options("logs")
+        if 'log_format' in options:
+            self.LOG_FORMAT = cf.get("logs", "log_format")
+        if self.LOG_FORMAT is None:
+            self.LOG_FORMAT = "%(asctime)s - %(name)s[func: %(funcName)s line:%(lineno)d] - %(levelname)s: %(message)s"
+        if 'log_level' in options:
+            log_level = cf.get("logs", "log_level")
+            if log_level not in ['debug', 'info']:
+                self.console_log_status = False
+            self.parse_log_level(log_level)
+        if 'log_dir' in options:
+            log_dir = cf.get("logs", "log_dir")
+            self.log_dir = log_dir
+        else:
+            self.log_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'logs')
+
+    def config_log(self):
+        """
+        配置日志
+        :return:
+        """
+        self.parse_config()
+
+        # chekc exists
+        if not self.check_log_dir(self.log_dir):
+            self.mkdir_log_dir(self.log_dir)
+
+        # self.init_log(log_path=self.log_dir, log_format=self.LOG_FORMAT)
+
+        log_path = f'{self.log_dir}/{self.logger_name}_logs.log'
+        self.init_log(log_path=log_path, log_format=self.LOG_FORMAT)
+
+    def init_log(self, log_path=None, log_format=None):
+        """
+        初始化日志
+        :param
+        """
+        self.logger = logging.getLogger(self.logger_name)
+        self.logger.setLevel(self.LOG_LEVEL)
+        self.logger.propagate = True
+
+        if log_path is None:
+            log_path = os.path.join()
+
+        if self.LOG_LEVEL in [logging.DEBUG, logging.INFO]:
+            self.console_log_status = True
+
+        if log_format is None:
+            log_format = "%(asctime)s - %(name)s[func: %(funcName)s line:%(lineno)d] - %(levelname)s: %(message)s"
+        format_str = logging.Formatter(log_format)
+
+        if self.console_log_status:
+            console_handle = logging.StreamHandler()
+            console_handle.setFormatter(format_str)
+            self.logger.addHandler(console_handle)
+
+        file_handle = handlers.TimedRotatingFileHandler(filename=log_path, when='D', backupCount=3,
+                                                        encoding='utf-8')
+        file_handle.setFormatter(format_str)
+        self.logger.addHandler(file_handle)
+
+    def debug(self, msg):
+        self.logger.debug(msg, stacklevel=self.stacklevel)
+
+    def info(self, msg):
+        self.logger.info(msg, stacklevel=self.stacklevel)
+
+    def warn(self, msg):
+        self.logger.warning(msg, stacklevel=self.stacklevel)
+
+    def error(self, msg):
+        self.logger.error(msg, stacklevel=self.stacklevel)
+
+    def critical(self, msg):
+        self.logger.critical(msg, stacklevel=self.stacklevel)
+
+
+if __name__ == '__main__':
+    LoggerBase("ss")
--- software-spider/software-spider.py
+++ software-spider/software-spider.py
@@ -1,19 +1,76 @@
 import requests
 import threading
+import logs
 from contextlib import closing
 from urllib.parse import urlparse
+from requests_html import HTMLSession
+
+LOG = logs.LoggerBase("software-spider")
+
 
 class SoftWareSpider(object):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                      "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.76"
-    }
+
+    def __init__(self):
+        self.threads = []
+        self.base_url = None
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                          "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.76"
+        }
+        self.params_list = []
+        self.url_list = []
+
+    def get_download_url(self):
+        pass
+
+    @staticmethod
+    def parse_url(url: str):
+        parse_url = urlparse(url)
+        filename = parse_url.path.split("/")[-1]
+        version = None
+        return {
+            'filename': filename,
+            'version': version
+        }
+
+    def download(self, url, filepath):
+        # 检查文件件是否存在
+        code_info = self.parse_url(url)
+        file_name = code_info.get('filename')
+        with closing(requests.get(url, stream=True)) as response:
+            chunk_size = 40960  # 单次请求最大值
+            content_size = int(response.headers['content-length'])  # 内容体总大小
+            data_count = 0
+            with open(f"{filepath}/{file_name}", "wb") as file:
+                for data in response.iter_content(chunk_size=chunk_size):
+                    file.write(data)
+                    data_count = data_count + len(data)
+                    now_jd = (data_count / content_size) * 100
+                    print("\r 文件下载进度：%d%%(%d/%d) - %s"
+                             % (now_jd, data_count, content_size, f"{filepath}/{file_name}"))
+                    if now_jd == 100:
+                        LOG.info("\r 文件下载完成：%d%%(%d/%d) - %s"
+                                 % (now_jd, data_count, content_size, f"{filepath}/{file_name}"))
+
+    def run(self):
+        # 采用多线程 下载
+        # url = self.url_list[0]
+        for url in self.url_list:
+            threading.Thread(target=self.download, args=(url, '.'))
+            self.threads.append(threading.Thread(target=self.download, args=(url, '.')))
+
+        for t in self.threads:
+            t.start()
+        for t in self.threads:
+            t.join()
 
 
 class Code(SoftWareSpider):
-    pass
 
     def __init__(self):
+        super().__init__()
+
+        self.threads = []
         self.data = {
             "build": "stable",
             "os": [
@@ -29,9 +86,6 @@ class Code(SoftWareSpider):
             ]
         }
         self.base_url = "https://code.visualstudio.com/sha/download"
-        self.params_list = []
-        self.url_list = []
-        self.threads = []
 
         self.gen_params()
         self.get_download_url()
@@ -52,34 +106,33 @@ class Code(SoftWareSpider):
             if r.status_code == 302:
                 self.url_list.append(r.headers['location'])
 
-    def download(self, url, filepath):
-        # 检查文件件是否存在
+    # def download(self, url, filepath):
+    #     # 检查文件件是否存在
+    #     code_info = self.parse_url(url)
+    #     file_name = code_info.get('filename')
+    #     with closing(requests.get(url, stream=True)) as response:
+    #         chunk_size = 40960  # 单次请求最大值
+    #         content_size = int(response.headers['content-length'])  # 内容体总大小
+    #         data_count = 0
+    #         with open(f"{filepath}/{file_name}", "wb") as file:
+    #             for data in response.iter_content(chunk_size=chunk_size):
+    #                 file.write(data)
+    #                 data_count = data_count + len(data)
+    #                 now_jd = (data_count / content_size) * 100
+    #                 LOG.info("\r 文件下载进度：%d%%(%d/%d) - %s"
+    #                          % (now_jd, data_count, content_size, f"{filepath}/{file_name}"), end=" ")
 
-        code_info = self.parse_url(url)
-        file_name = code_info.get('filename')
-        with closing(requests.get(url, stream=True)) as response:
-            chunk_size = 4096  # 单次请求最大值
-            content_size = int(response.headers['content-length'])  # 内容体总大小
-            data_count = 0
-            with open(f"{filepath}/{file_name}", "wb") as file:
-                for data in response.iter_content(chunk_size=chunk_size):
-                    file.write(data)
-                    data_count = data_count + len(data)
-                    now_jd = (data_count / content_size) * 100
-                    print("\r 文件下载进度：%d%%(%d/%d) - %s"
-                          % (now_jd, data_count, content_size, f"{filepath}/{file_name}"), end=" ")
-
-    def run(self):
-        # 采用多线程 下载
-        # url = self.url_list[0]
-        for url in self.url_list:
-            threading.Thread(target=self.download, args=(url, '.'))
-            self.threads.append(threading.Thread(target=self.download, args=(url, '.')))
-
-        for t in self.threads:
-            t.start()
-        for t in self.threads:
-            t.join()
+    # def run(self):
+    #     # 采用多线程 下载
+    #     # url = self.url_list[0]
+    #     for url in self.url_list:
+    #         threading.Thread(target=self.download, args=(url, '.'))
+    #         self.threads.append(threading.Thread(target=self.download, args=(url, '.')))
+    #
+    #     for t in self.threads:
+    #         t.start()
+    #     for t in self.threads:
+    #         t.join()
 
     # def parse_version(self, filename: str):
     #     if filename.endswith('deb'):
@@ -94,15 +147,36 @@ class Code(SoftWareSpider):
     #         print(f"the filename {filename} is not in ['deb', 'rpm', 'exe']")
     #     pass
 
-    def parse_url(self, url: str):
-        parse_url = urlparse(url)
-        filename = parse_url.path.split("/")[-1]
-        version = None
-        return {
-            'filename': filename,
-            'version': version
-        }
+    # @staticmethod
+    # def parse_url(url: str):
+    #     parse_url = urlparse(url)
+    #     filename = parse_url.path.split("/")[-1]
+    #     version = None
+    #     return {
+    #         'filename': filename,
+    #         'version': version
+    #     }
+    #
+
+
+class Git(SoftWareSpider):
+
+    def __init__(self):
+        super().__init__()
+        self.base_url = "https://git-scm.com/download/win"
+        self.get_download_url()
+        self.run()
+
+    def get_download_url(self):
+        session = HTMLSession()
+        r = session.get(self.base_url)
+        win_git_x86 = r.html.xpath('//*[@id="main"]/p[2]/strong/a', first=True)
+
+        win_git_x64 = r.html.xpath('//*[@id="main"]/p[3]/strong/a', first=True)
+
+        self.url_list.extend([win_git_x64.attrs['href'], win_git_x86.attrs['href']])
 
 
 if __name__ == '__main__':
-    Code()
+    # Code()
+    Git()
