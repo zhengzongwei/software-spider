8eff23f19dd025c29a2b83c622211302d8820d03
zhengzongwei <zhengzongwei@foxmail.com>
2023-01-17 11:25:40 +0800

    添加obs下载

--- software_spider/cores/cores.py
+++ software_spider/cores/cores.py
@@ -16,13 +16,16 @@ LOG = logs.Logger("software_spider")
 class SoftwareSpider(object):
     def __init__(self) -> None:
         self.threads = []
+        self.url_info_list = []
         self.url_list = []
         self.soft_code_conf = dict()
         self.base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        self.download_path = os.path.join(os.path.dirname(self.base_dir), 'downloads')
+        self.download_base_path = os.path.join(os.path.dirname(self.base_dir), 'downloads')
+        # self.download_path = os.path.join(os.path.dirname(self.base_dir), 'downloads')
         self.headers = {
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:108.0) Gecko/20100101 Firefox/108.0"
         }
+        # TODO 需要支持 更新指定软件
         self.run()
 
     @staticmethod
@@ -60,10 +63,14 @@ class SoftwareSpider(object):
             file_path = os.path.join(self.base_dir, 'conf', 'software.yaml')
         with open(file_path, 'r') as f:
             self.soft_code_conf = yaml.safe_load(f).get('software')
-            LOG.info(f'配置 {self.soft_code_conf}')
 
     def get_download_url(self) -> None:
         for software_name in self.soft_code_conf:
+            url_info = {
+                'name': software_name,
+                'urls': list()
+
+            }
             base_url = self.soft_code_conf[software_name]['base_url']
 
             if 'os' in self.soft_code_conf[software_name]:
@@ -72,7 +79,7 @@ class SoftwareSpider(object):
                 for param in param_list:
                     r = requests.get(base_url, params=param, headers=self.headers, allow_redirects=False)
                     if r.status_code == 302:
-                        self.url_list.append(r.headers['location'])
+                        url_info['urls'].append(r.headers['location'])
 
             if 'xpath' in self.soft_code_conf[software_name]:
                 r = requests.get(base_url, headers=self.headers, allow_redirects=False)
@@ -80,36 +87,37 @@ class SoftwareSpider(object):
                     session = HTMLSession()
                     r = session.get(base_url)
                     for xpath in self.soft_code_conf[software_name]['xpath']:
-                        self.url_list.append(r.html.xpath(xpath, first=True).attrs['href'])
+                        url_info['urls'].append(r.html.xpath(xpath, first=True).attrs['href'])
 
             if 'download_url' in self.soft_code_conf[software_name]:
                 for download_url in self.soft_code_conf[software_name]['download_url']:
                     download_url = f'{base_url}/{download_url}'
                     r = requests.get(download_url, headers=self.headers, allow_redirects=False)
                     if r.status_code == 302:
-                        self.url_list.append(r.headers['location'])
+                        url_info['urls'].append(r.headers['location'])
+            self.url_info_list.append(url_info)
 
-    def download(self, url, filepath) -> None:
+    def download(self, url, download_path) -> None:
         software_info = self.parse_url(url)
         file_name = software_info.get('file_name')
         with closing(requests.get(url, stream=True)) as response:
             chunk_size = 40960  # 单次请求最大值
             content_size = int(response.headers['content-length'])  # 内容体总大小
             data_count = 0
-            with open(f"{filepath}/{file_name}", "wb") as file:
+            with open(f"{download_path}/{file_name}", "wb") as file:
                 for data in response.iter_content(chunk_size=chunk_size):
                     file.write(data)
                     data_count = data_count + len(data)
                     now_jd = (data_count / content_size) * 100
                     print("\r 文件下载进度：%d%%(%d/%d) - %s"
-                          % (now_jd, data_count, content_size, f"{filepath}/{file_name}"), end=" ")
+                          % (now_jd, data_count, content_size, f"{download_path}/{file_name}"), end=" ")
                     if now_jd == 100:
                         LOG.info("\r 文件下载完成：%d%%(%d/%d) - %s"
-                                 % (now_jd, data_count, content_size, f"{filepath}/{file_name}"))
+                                 % (now_jd, data_count, content_size, f"{download_path}/{file_name}"))
 
     def pre_run(self) -> None:
         # 检查下载文件夹是否创建
-        self.check_dir(self.download_path)
+        self.check_dir(self.download_base_path)
         # 检查配置文件内容
 
         self.get_config()
@@ -122,21 +130,26 @@ class SoftwareSpider(object):
 
     def run(self) -> None:
         self.pre_run()
-        LOG.info(f"开始下载任务 {len(self.url_list)}")
-        for url in self.url_list:
-            # 检查软件是否已下载
-            software_info = self.parse_url(url)
-            if not software_info['version']:
-                software_info['version'] = 'latest'
+        LOG.info(f"开始下载任务 {len(self.url_info_list)}")
+        for url_info in self.url_info_list:
+            LOG.info(url_info)
+            name = url_info['name']
 
-            # self.download_path = os.path.join(self.download_path,software_info['version'])
-            if os.path.exists(os.path.join(self.download_path, software_info['file_name'])):
-                LOG.info(f"软件 {software_info['file_name']}已下载！")
-                continue
+            for url in url_info['urls']:
+                software_info = self.parse_url(url)
+                if not software_info['version']:
+                    software_info['version'] = 'latest'
 
-            LOG.info(f"开始下载软件 {url}")
-            threading.Thread(target=self.download, args=(url, self.download_path))
-            self.threads.append(threading.Thread(target=self.download, args=(url, self.download_path)))
+                download_path = os.path.join(self.download_base_path, name, software_info['version'])
+                self.check_dir(download_path)
+                if os.path.exists(
+                        os.path.join(download_path, name, software_info['version'], software_info['file_name'])):
+                    LOG.info(f"软件 {software_info['file_name']}已下载！")
+                    continue
+
+                LOG.info(f"开始下载软件 {name} {software_info['file_name']}")
+                threading.Thread(target=self.download, args=(url, download_path))
+                self.threads.append(threading.Thread(target=self.download, args=(url, download_path)))
 
         for t in self.threads:
             t.start()
